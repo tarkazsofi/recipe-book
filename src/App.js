ecebf4ab4a6748b5d0c9ef67de339c157994d7f7
Zsofia Tarkanyi <zsofi.t@gmail.com>
2022-01-27 19:57:55 +0100

    Added router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
+import recipes from "./recipes";
+import { Link } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import Settings from "./Settings";
 
 const App = () => {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="App">
+      <h1>Recipe Book</h1>
+      <Link to="/settings">Settings</Link>
+      <Routes>
+        <Route path="settings" element={<Settings />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/recipes.js
@@ -0,0 +1,34 @@
+const recipes = [
+  {
+    name: "My favourite dish",
+    ingredients: [
+      {
+        name: "potatoes",
+        amount: "1",
+        unit: "kg",
+      },
+      {
+        name: "bacon",
+        amount: "0.25",
+        unit: "kg",
+      },
+    ],
+  },
+  {
+    name: "My favourite cake",
+    ingredients: [
+      {
+        name: "sugar",
+        amount: "1",
+        unit: "kg",
+      },
+      {
+        name: "flour",
+        amount: "0.25",
+        unit: "kg",
+      },
+    ],
+  },
+];
+
+export default recipes;
